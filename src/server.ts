ccdb165c11598601e303149739b98ba46eeada48
BitWizCoder <mhmdnoman01@gmail.com>
2025-12-26 02:32:35 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 5000;
+app.use(express.json());
+
+app.post("/users", async (req, res) => {
+  const body = req.body;
+
+  res.status(200).json({
+    message: body,
+  });
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "This is the root route.",
+    path: req.path,
+  });
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port:", port);
+});
