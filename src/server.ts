a90867706514f4a86a001bed6b72e0f652de3465
BitWizCoder <mhmdnoman01@gmail.com>
2025-12-26 23:02:06 +0600

    added db

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,34 @@
 import express, { Request, Response } from "express";
 import { Pool } from "pg";
+import dotenv from "dotenv";
+import path from "path";
 
 const app = express();
 const port = 5000;
 app.use(express.json());
 
-const pool = new Pool();
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+const pool = new Pool({
+  connectionString: `${process.env.CONNECTION_STR}`,
+});
+
+const initDB = async () => {
+  await pool.query(/* SQL */ `
+    CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(250) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    age INT, 
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+    )
+    `);
+  console.log("Database Connected");
+};
+
+initDB();
 
 app.post("/users", async (req, res) => {
   const body = req.body;
